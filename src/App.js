c4151806d01f3bc20e31ecad4da16d88d3511209
pushpanjali29 <pushpanjali.0029@gamil.com>
2024-12-07 19:23:52 +0530

    initial commit for react project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+
+const customDictionary = {
+
+  teh: "the",
+
+  wrok: "work",
+
+  fot: "for",
+
+  exampl: "example"
+
+};
+
 
 function App() {
-  return (
+  const[inputText , setInputText] = useState("");
+  const [suggestion , setSuggestion] = useState("");
+
+  const handleInputChange = (event) => {
+    const text = event.target.value;
+    setInputText(text);
+
+  const words = text.toLowerCase().split(/\s+/);
+  const firstWrongWord = words.find(word => customDictionary[word]);
+
+  if(firstWrongWord)
+  {
+    const correctWord = customDictionary[firstWrongWord];
+    setSuggestion(`Did you mean: ${correctWord}?`);
+  }
+  else{
+    setSuggestion("");
+  }
+};
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Spell Check and auto-Correction
+        </h1>
+        <br/>
+        <textarea type='text' placeholder='Enter text...' 
+        value={inputText}
+        onChange={handleInputChange}>
+        </textarea>
+        {suggestion && (
+        <div style={{ marginTop: "10px", color: "black", fontWeight: "bold" }}>
+          {suggestion}
+        </div>
+      )}
     </div>
   );
 }
